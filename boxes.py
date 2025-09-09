e029b86c77b208af05487fa4b7a6ac6419b7d7ce
Ludvigeden <ludvig.edenlund05@hotmail.com>
2025-09-10 01:56:23 +0200

    klar

--- boxes.py
+++ boxes.py
@@ -1,1 +1,13 @@
-print("hej") #test
+#############################
+#INLÄMNINGSUPPGIFT
+#MMS325 programmering och algoritmiskt tänkande
+#HT25
+#Ludvig Edenlund (2005,02,03)
+#Edvin Olausson (2003,07,03)
+#########################################
+
+#Huvudprogrammet startar här
+
+#------------------------------
+
+#-----------------------------
